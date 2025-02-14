45e64e8017c1e1e90b7edea92819b0e53d3f8725
Thereza2002 <reza.javadian@student.uclouvain.be>
2025-02-14 16:09:24 +0100

    implementationProblem

--- importHydrogen.py
+++ importHydrogen.py
@@ -6,14 +6,24 @@ import pyomo.environ as pyo
 model = pyo.ConcreteModel()
 
 # Define model parameters
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+maxVboat = 200000 # m^3
+
+LHV_NH3 = 18.5 # MJ/kg
+rho_NH3 = 600 # kg/m^3
+loss_NH3 = 0.4 # %
+H2_NH3 = 0.18 # t_H2/t_NH3
+
+LHV_CH4 = 18.5 # MJ/kg
+rho_CH4 = 600 # kg/m^3
+loss_CH4 = 0.35 # %
+H2_CH4 = 0.25 # t_H2/t_CH4
+CO2_CH4_needs = 2.75 # t
+
+
 
 # Define model variables
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+model.b_NH3 = pyo.Var(within = pyo.NonNegativeReals)
+model.b_CH4 = pyo.Var(within = pyo.NonNegativeReals)
 
 # Define the objective functions
 ##########################################
@@ -26,7 +36,7 @@ model = pyo.ConcreteModel()
 ##########################################
 
 # Specify the path towards your solver (gurobi) file
-solver = pyo.SolverFactory('...')
+solver = pyo.SolverFactory('/Users/rezajavadian/Documents/gurobi')
 sol = solver.solve(model)
 
 # Print here the number of CH4 boats and NH3 boats
